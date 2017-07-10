850043de098bdc65a7fa93908c654ba4a779466f
edanaaa <edana.gonzalez12@gmail.com>
2017-07-10 10:56:33 -0700

    Signed-off-by: edanaaa <edana.gonzalez12@gmail.com>

---
+++ Hmwk/Hello World Program/main.cpp
@@ -0,0 +1,34 @@
+/* 
+ * File:   main.cpp
+ * Author: Edana Gonzalez
+ * Created on June 20, 2017, 10:43 PM
+ * Purpose:  Our first program
+ */
+
+//System Libraries
+#include <iostream>  //Input - Output Library
+using namespace std; //Name-space under which system libraries exist
+
+//User Libraries
+
+//Global Constants
+
+//Function Prototypes
+
+//Execution begins here
+int main(int argc, char** argv) 
+{
+    //Declare variables
+    
+    //Initialize variables
+    
+    //Input data
+    
+    //Map inputs to outputs or process the data
+    
+    //Output the transformed data
+    cout<<"Hello World"<<endl;
+    
+    //Exit stage right!
+    return 0;
+}
